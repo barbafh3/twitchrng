f0450c152d858dba66734f9bbf4581b439e88512
barbafh <barbafh@gmail.com>
2019-07-03 22:50:58 -0300

    Functionality reestabilished after moving to React Hooks

--- src/components/stream/StreamShow.js
+++ src/components/stream/StreamShow.js
@@ -1,61 +1,87 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { setPlayerState, fetchStreams } from "../../actions/stream";
+import {
+  setPlayerState,
+  fetchStreams,
+  setUser,
+  setDelay
+} from "../../actions/stream";
 import { twitchAuth } from "../../actions/session";
 
 const StreamShow = () => {
   const dispatch = useDispatch();
 
+  const user = useSelector(state => state.streams.user);
   const streams = useSelector(state => state.streams.streamList);
   const playerState = useSelector(state => state.streams.playerState);
   const accessToken = useSelector(state => state.session.twitchToken);
+  const delay = useSelector(state => state.streams.delay);
 
-  const reloadTimeout = ms => {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  // Creates the twitch player
+  const setEmbed = (ebd, usr) => {
+    ebd = new window.Twitch.Embed("twitch_embed", {
+      align: "center",
+      width: "854",
+      height: "480",
+      channel: usr,
+      layout: "video"
+    });
   };
 
-  const renderTwitch = async () => {
-    let currentUser = "";
-    let rng = null;
-    let user = "";
-    let embed;
-
-    const parentEl = document.createElement("div");
-    parentEl.setAttribute("id", "twitch_embed");
-    await document.body.appendChild(parentEl);
-
-    while (playerState) {
-      parentEl.innerHTML = "";
-      document.getElementById("twitch_embed").innerHTML = "";
-      rng = Math.trunc(Math.random() * 20);
-      user = streams[rng].user_name;
-      let timeout = 60000;
-      // let timeout = 1.8e+6;
-      if (user !== currentUser) {
-        const script = document.createElement("script");
-        script.setAttribute("src", "https://embed.twitch.tv/embed/v1.js");
-        script.addEventListener("load", () => {
-          embed = new window.Twitch.Embed("twitch_embed", {
-            align: "center",
-            width: "854",
-            height: "480",
-            channel: user,
-            layout: "video"
-          });
-        });
-        parentEl.appendChild(script);
-        currentUser = user;
-        await reloadTimeout(timeout);
-      }
+  // Clears and renders/re-renders the player
+  // created by the setEmbed function
+  const renderTwitch = () => {
+    if (user) {
+      let twitchTag = document.getElementById("twitch_embed");
+      twitchTag.innerHTML = "";
+      let embed;
+      let parentEl = document.createElement("div");
+      parentEl.setAttribute("id", "twitch_embed");
+      document.body.appendChild(parentEl);
+      let script = document.createElement("script");
+      script.setAttribute("src", "https://embed.twitch.tv/embed/v1.js");
+      script.addEventListener("load", setEmbed(embed, user));
+      parentEl.appendChild(script);
     }
   };
 
-  const changePlayerState = async () => {
-    await dispatch(setPlayerState(!playerState));
+  // Gets a random user from the streams list
+  // and sets it on the store
+  const randomUser = async () => {
+    let rng = Math.trunc(Math.random() * 20);
+    let newUser = streams[rng].user_name;
+    await dispatch(setUser(newUser));
   };
 
+  // Runs once when component mounts.
+  // Gets the auth token from twitch and
+  // gets the list of streams
+  useEffect(() => {
+    (async () => {
+      await dispatch(twitchAuth());
+      await dispatch(fetchStreams(accessToken));
+    })();
+  }, []);
+
+  // Sets an interval to randomize the user
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (playerState) {
+        randomUser();
+        dispatch(setDelay(60000));
+      }
+    }, delay);
+    return () => clearInterval(interval);
+  });
+
+  // Re-renders the player everytime
+  // the user changes
+  useEffect(() => {
+    renderTwitch();
+  }, [user]);
+
+  // Renders the randomize on/off button
   const renderRngButton = () => {
     let text;
     let classes = "ui button ";
@@ -71,31 +97,21 @@ const StreamShow = () => {
     }
     return (
       <p>
-        <button className={classes} onClick={() => changePlayerState()}>
+        <button className={classes} onClick={handleClick}>
           <i className={icon} /> {text}
         </button>
       </p>
     );
   };
 
-  const sleep = milliseconds => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  // Toggles the playerState and randomizes
+  // the user if playerState is set to true
+  const handleClick = async () => {
+    await dispatch(setPlayerState(!playerState));
+    if (!playerState) {
+      randomUser();
+    }
   };
-
-  const startup = async () => {
-    await dispatch(twitchAuth());
-    await dispatch(fetchStreams(accessToken));
-    await changePlayerState();
-  };
-
-  // Runs once when component mounts
-  useEffect(() => {
-    startup();
-  }, []);
-
-  useEffect(() => {
-    renderTwitch();
-  }, [playerState]);
 
   if (streams) {
     return (
@@ -106,7 +122,7 @@ const StreamShow = () => {
       </div>
     );
   } else {
-    return <div>Loading...</div>;
+    return <></>;
   }
 };
 
--- src/components/stream/TagForm.js
+++ src/components/stream/TagForm.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
-import { Field, reduxForm } from 'redux-form';
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
 
 class TagForm extends Component {
+  renderInput = ({ input, label }) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+      </div>
+    );
+  };
 
-    renderInput = ({ input, label }) => {
-        return (
-            <div>
-                <label>{label}</label>
-                <input {...input} autoComplete='off' />
-            </div>
-        )
-    }
+  onSubmit = formValues => {
+    this.props.onSubmit(formValues);
+  };
 
-    onSubmit = formValues => {
-        this.props.onSubmit(formValues);
-    }
-
-    render(){
-        return (
-            <div>
-                <form onSubmit={this.props.handleSubmit(this.onSubmit)} className='ui form error'>
-                    <Field name='tag' component={this.renderInput} label='Enter tag' />
-                    <button className='ui button primary'>Search</button>
-                </form>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          className="ui form error"
+        >
+          <Field name="tag" component={this.renderInput} label="Enter tag" />
+          <button className="ui button primary">Search</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default reduxForm({ form: 'streamForm' })(TagForm);
+export default reduxForm({ form: "streamForm" })(TagForm);
